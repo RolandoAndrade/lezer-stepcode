5de4fc0528fc301eaa72043e1b7fb5771b7938b7
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-12-10 19:19:41 -0400

    Grammar v0.1.0

---
+++ src/tokens.js
@@ -0,0 +1,43 @@
+import * as terms from "./parser.terms.js"
+
+const keywordsMap = new Map([
+  ['proceso', terms.proceso],
+  ['finproceso', terms.finproceso],
+  ['definir', terms.definir],
+  ['como', terms.como],
+  ['escribir', terms.escribir],
+  ['leer', terms.leer],
+  ['si', terms.si],
+  ['sino', terms.sino],
+  ['entonces', terms.entonces],
+  ['finsi', terms.finsi],
+  ['segun', terms.segun],
+  ['hacer', terms.hacer],
+  ['finsegun', terms.finsegun],
+  ['entero', terms.entero],
+  ['real', terms.real],
+  ['logico', terms.logico],
+  ['cadena', terms.cadena],
+  ['caracter', terms.caracter],
+  ['mod', terms.mod],
+  ['div', terms.div],
+  ['mientras', terms.mientras],
+  ['finmientras', terms.finmientras],
+  ['para', terms.para],
+  ['finpara', terms.finpara],
+  ['hasta', terms.hasta],
+  ['repetir', terms.repetir],
+  ['subproceso', terms.subproceso],
+  ['finsubproceso', terms.finsubproceso],
+  ['funcion', terms.funcion],
+  ['finfuncion', terms.finfuncion],
+  ['romper', terms.romper],
+  ['continuar', terms.continuar],
+  ['retornar', terms.retornar],
+])
+
+export function keywords(name) {
+  name = name?.toLowerCase()
+  return keywordsMap.has(name) ? keywordsMap.get(name) : -1
+}
+
