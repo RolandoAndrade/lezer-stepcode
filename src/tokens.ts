f0e36c419a89dea574003600431dbd1fa8f3fa51
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-12-10 13:31:40 -0400

    Added more elements

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,3 +1,5 @@
+import {ExternalTokenizer, ContextTracker} from "@lezer/lr"
+
 const keywordsMap = new Map([
   ['proceso', proceso],
   ['finproceso', finproceso],
@@ -23,3 +25,16 @@ export function keywords(name: string) {
   name = name?.toLowerCase()
   return keywordsMap.has(name) ? keywordsMap.get(name) : -1
 }
+
+export const otherwiseToken = new ExternalTokenizer((input, stack) => {
+  let {next} = input
+  if (next == plus || next == minus) {
+    input.advance()
+    if (next == input.next) {
+      input.advance()
+      let mayPostfix = !stack.context && stack.canShift(incdec)
+      input.acceptToken(mayPostfix ? incdec : incdecPrefix)
+    }
+  }
+  return -1
+}, {contextual: true})
